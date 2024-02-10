06ac6e141f57c2382efbfdc37f8cacef628b46c7
memento <claude.rc@gmail.com>
2024-02-10 17:06:58 -0600

    (dbg) compilation details

--- libsecp256k1_py_bindings/keys.py
+++ libsecp256k1_py_bindings/keys.py
@@ -413,7 +413,7 @@ class PublicKey:
 
         return PublicKey(public_key, context)
 
-    def format(self, compressed: bool = True) -> bytes:  # noqa: A003
+    def format(self, compressed: bool = True) -> bytes:
         """
         Format the public key.
 
@@ -585,7 +585,7 @@ class PublicKeyXOnly:
 
         return cls(xonly_pubkey, parity=not not pk_parity[0], context=context)
 
-    def format(self) -> bytes:  # noqa: A003
+    def format(self) -> bytes:
         """Serialize the public key.
 
         :return: The public key serialized as 32 bytes.
