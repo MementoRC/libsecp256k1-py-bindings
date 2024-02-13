8854504b86258f3fd18eb53cf62a0de4d4dff5b6
memento <claude.rc@gmail.com>
2024-02-12 19:20:45 -0600

    (dbg)

--- tests/test_bench.py
+++ tests/test_bench.py
@@ -1,5 +1,6 @@
-from libsecp256k1_py_bindings import PrivateKey, PublicKey, verify_signature
 from samples import MESSAGE, PRIVATE_KEY_BYTES, PUBLIC_KEY_COMPRESSED, SIGNATURE
+
+from libsecp256k1_py_bindings import PrivateKey, PublicKey, verify_signature
 
 
 def test_verify_signature_util(benchmark):
--- tests/test_ecdsa.py
+++ tests/test_ecdsa.py
@@ -1,5 +1,6 @@
-from libsecp256k1_py_bindings.ecdsa import cdata_to_der, der_to_cdata
 from samples import SIGNATURE
+
+from libsecp256k1_py_bindings.ecdsa import cdata_to_der, der_to_cdata
 
 
 def test_der():
