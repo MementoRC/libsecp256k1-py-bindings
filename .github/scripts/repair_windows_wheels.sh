721c3a97abaf914a360e70689acb290cb51cf57b
memento <claude.rc@gmail.com>
2024-02-15 16:49:20 -0600

    (ref) always some silliness

--- .github/scripts/repair_windows_wheels.sh
+++ .github/scripts/repair_windows_wheels.sh
@@ -9,7 +9,7 @@ DEST_DIR=$2
 # By default, the Windows wheels are not repaired.
 # In this case, we need to vendor VCRUNTIME140.dll
 wheel unpack "$WHEEL"
-WHEEL_DIRNAME=$(ls -d libsecp256k1-py-bindings-*)
+WHEEL_DIRNAME=$(ls -d libsecp256k1_py_bindings-*)
 python .github/data/vendor.py "$WHEEL_DIRNAME"
 wheel pack "$WHEEL_DIRNAME" -d "$DEST_DIR"
 rm -rf "$WHEEL_DIRNAME"
